1129e659198a812e8bc311127e002cfbecb3c9e9
Cecil Jones <dewdrops1800@outlook.com>
2022-03-13 23:35:12 +0000

    sanity and Main interface check done

--- __tests__/polynomial.test.js
+++ __tests__/polynomial.test.js
@@ -1,3 +1,15 @@
+//Sanity Check
 test("Sanity check", () => {
     expect(true).toBe(true);
+});
+
+// Testing the interface
+const {Polynomial} = require("../polynomial");
+
+describe("Polynomial", () => {
+  const polynomial = new Polynomial();
+
+  test("defines Main()", () => {
+    expect(typeof polynomial.Main).toBe("function");
+  });
 });
